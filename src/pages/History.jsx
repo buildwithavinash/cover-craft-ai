0738dc90b78df0f962319d835de565ef3b335643
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-04 11:31:28 +0530

    feat: delete and clear all from history

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -13,13 +13,25 @@ const History = () => {
     setData(stored);
   }, []);
 
+
+  const handleDelete = (id) => {
+    const updated = data.filter((item)=> item.id !== id);
+    setData(updated);
+    localStorage.setItem("results", JSON.stringify(updated));
+  }
   return (
     <div>
         <div>
             <h2>Your History</h2>
 
+            <button onClick={()=>{localStorage.removeItem("results"); setData([])}}>Clear All</button>
+
             {data.map((d)=>(
                 <div key={d.id} onClick={()=> navigate(`/detail/${d.id}`)} className="border border-slate-300 p-2">
+
+                    <button onClick={(e)=>{e.stopPropagation(); handleDelete(d.id); }}>
+                        <i class="ri-delete-bin-6-line"></i>
+                    </button>
                     <p className="line-clamp-2">{new Date(d.createdAt).toLocaleString()}</p>
 
                     <p className="line-clamp-2">{d.text}</p>
